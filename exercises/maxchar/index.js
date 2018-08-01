c34988fce391275e48d859163e440191a6ab42d6
Logan Hart <2loganhart@gmail.com>
2018-08-01 16:33:22 -0700

    maxchar - all tests pass

--- exercises/maxchar/index.js
+++ exercises/maxchar/index.js
@@ -5,6 +5,24 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-function maxChar(str) {}
+function maxChar(str) {
+  const charMap = {};
+  let max = 0;
+  let maxChar = '';
+  str.slice(0).split('').forEach((c) => {
+    if (charMap[c]) {
+      charMap[c] += 1;
+    } else {
+      charMap[c] = 1;
+    }
+  });
+  for (let k in charMap) {
+    if (charMap[k] > max) {
+      max = charMap[k];
+      maxChar = k;
+    }
+  }
+  return maxChar;
+}
 
 module.exports = maxChar;
