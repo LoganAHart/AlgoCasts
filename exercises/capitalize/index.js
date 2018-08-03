d4860f5471ed3a28e8e91574cc421e662185432e
Logan Hart <2loganhart@gmail.com>
2018-08-02 18:19:36 -0700

    capitalize - all tests pass

--- exercises/capitalize/index.js
+++ exercises/capitalize/index.js
@@ -7,6 +7,27 @@
 //   capitalize('a lazy fox') --> 'A Lazy Fox'
 //   capitalize('look, it is working!') --> 'Look, It Is Working!'
 
-function capitalize(str) {}
+function capitalize(str) {
+  return str
+    .split(' ')
+    .map((word) => {
+      const chars = word.split('');
+      chars[0] = chars[0].toUpperCase();
+      return chars.join('');
+    })
+    .join(' ');
+}
+
+function capitalizeAlt(str) {
+  let result = str[0].toUpperCase();
+  for (let i = 1; i < str.length; i++) {
+    if (str[i - 1] === ' ') {
+      result += str[i].toUpperCase();
+    } else {
+      result += str[i];
+    }
+  }
+  return result;
+}
 
 module.exports = capitalize;
