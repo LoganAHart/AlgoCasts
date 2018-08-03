fb154af502e6fbe340c8ec06fc8cca9272314540
Logan Hart <2loganhart@gmail.com>
2018-08-02 17:32:12 -0700

    chunk - all tests pass

--- exercises/chunk/index.js
+++ exercises/chunk/index.js
@@ -8,6 +8,14 @@
 // chunk([1, 2, 3, 4, 5], 4) --> [[ 1, 2, 3, 4], [5]]
 // chunk([1, 2, 3, 4, 5], 10) --> [[ 1, 2, 3, 4, 5]]
 
-function chunk(array, size) {}
+function chunk(array, size) {
+  const input = array.slice(0);
+  const result = [];
+  while (input.length) {
+    const current = input.splice(0, size);
+    result.push(current);
+  }
+  return result;
+}
 
 module.exports = chunk;
