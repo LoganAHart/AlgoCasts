232b9980331cdade9199d33d94f61eea0a612de4
Logan Hart <2loganhart@gmail.com>
2018-08-22 14:51:53 -0700

    levelwidth - all tests pass - using modified BF tree traversal

--- exercises/levelwidth/index.js
+++ exercises/levelwidth/index.js
@@ -11,6 +11,21 @@
 // 4       5
 // Answer: [1, 3, 2]
 
-function levelWidth(root) {}
+function levelWidth(root) {
+  const FLAG = 'flag';
+  const arr = [root, FLAG];
+  const levels = [0];
+  while (arr.length > 1) {
+    const node = arr.shift();
+    if (node === FLAG) {
+      levels.push(0);
+      arr.push(FLAG);
+    } else {
+      arr.push(...node.children);
+      levels[levels.length - 1]++;
+    }
+  }
+  return levels;
+}
 
 module.exports = levelWidth;
